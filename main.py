998384f6cd572c75e9aa263210fa69c8f413e0ec
Darleanow <99553875+Darleanow@users.noreply.github.com>
2022-05-11 11:36:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import choice
+flag=False
 chars="abcdefghijklmnopqrstuvwxyz"
 number="0123456789"
 specialchar="&#{'([-|_\^@)]}$*%/?!<>²"
@@ -11,18 +12,28 @@ if specialchars=="O":
 if majuscules=="O":
     possib.append("majuscules")
 passw=""
-for i in range(lenght):
-    choix=choice(possib)
-    if choix=="char":
-        choix=choice(chars)
-        passw+=choix
-    elif choix=="specialchars":
-        choix=choice(specialchar)
-        passw+=choix
-    elif choix=="majuscules":
-        choix=choice(chars)
-        passw+=choix.upper()
-    elif choix=="number":
-        choix=choice(number)
-        passw+=choix
-print(passw)
+reponse=""
+while flag==False:
+    for i in range(lenght):
+        choix=choice(possib)
+        if choix=="char":
+            choix=choice(chars)
+            passw+=choix
+        elif choix=="specialchars":
+            choix=choice(specialchar)
+            passw+=choix
+        elif choix=="majuscules":
+            choix=choice(chars)
+            passw+=choix.upper()
+        elif choix=="number":
+            choix=choice(number)
+            passw+=choix
+    while reponse != "O" or reponse !="N":
+        reponse=input(f"Est ce que le mot de passe '{passw}' vous convient ? [O/n] : ").upper()
+        if reponse=="O" or reponse=="N":
+            break
+    if reponse=="O":
+        flag=True
+    elif reponse=="N":
+        flag=False
+        passw=""
